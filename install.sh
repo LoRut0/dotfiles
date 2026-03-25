3c4b20a873daf4baf93b53b3d16b48b9f61bafc1
LoRut0 <yaemkapustu37@gmail.com>
2026-03-25 13:30:52 +0300

    sync

--- install.sh
+++ install.sh
@@ -22,12 +22,12 @@ done
 for file in $DOTFILES/home/.*; do
     name="$(basename "$file")"
 
-    if name==".config"; then
+    if [[ $name == ".config" ]]; then
         continue
     fi
 
     target="$HOME/$name"
-    if [ -e "$target" ]; then
+    if [[ -e "$target" ]]; then
         echo "Skipping $target (already exists)"
         continue
     fi
@@ -35,3 +35,10 @@ for file in $DOTFILES/home/.*; do
     ln -sfn "$file" "$target"
     echo "Linked dotfiles/home/$name to $target"
 done
+
+if [[ ! -e "$HOME/.zsh" ]]; then
+    mkdir "$HOME/.zsh"
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.zsh/zsh-autosuggestions"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh/zsh-syntax-highlighting"
+    git clone https://github.com/jeffreytse/zsh-vi-mode.git "$HOME/.zsh/zsh-vi-mode"
+fi
